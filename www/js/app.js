54b4a5c95e0609bce2f340893e0faf6c029d40d7
chidi.uba <talk2chidicfc@yahoo.com>
2016-01-30 11:39:44 +0000

    change title of app, add states and controller

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('myPage', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,85 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('signin', {
+      url: '/sign-in',
+      templateUrl: 'templates/sign-in.html',
+      controller: 'SignInCtrl'
+    })
+    .state('forgotpassword', {
+      url: '/forgot-password',
+      templateUrl: 'templates/forgot-password.html'
+    })
+    // .state('tabs', {
+    //   url: '/tab',
+    //   abstract: true,
+    //   templateUrl: 'templates/tabs.html'
+    // })
+    // .state('tabs.home', {
+    //   url: '/home',
+    //   views: {
+    //     'home-tab': {
+    //       templateUrl: 'templates/home.html',
+    //       controller: 'HomeTabCtrl'
+    //     }
+    //   }
+    // })
+    // .state('tabs.facts', {
+    //   url: '/facts',
+    //   views: {
+    //     'home-tab': {
+    //       templateUrl: 'templates/facts.html'
+    //     }
+    //   }
+    // })
+    // .state('tabs.facts2', {
+    //   url: '/facts2',
+    //   views: {
+    //     'home-tab': {
+    //       templateUrl: 'templates/facts2.html'
+    //     }
+    //   }
+    // })
+    // .state('tabs.about', {
+    //   url: '/about',
+    //   views: {
+    //     'about-tab': {
+    //       templateUrl: 'templates/about.html'
+    //     }
+    //   }
+    // })
+    // .state('tabs.navstack', {
+    //   url: '/navstack',
+    //   views: {
+    //     'about-tab': {
+    //       templateUrl: 'templates/nav-stack.html'
+    //     }
+    //   }
+    // })
+    // .state('tabs.contact', {
+    //   url: '/contact',
+    //   views: {
+    //     'contact-tab': {
+    //       templateUrl: 'templates/contact.html'
+    //     }
+    //   }
+    // });
+
+
+   $urlRouterProvider.otherwise('/sign-in');
+
+})
+
+.controller('SignInCtrl', function($scope, $state) {
+
+  $scope.signIn = function(user) {
+    console.log('Sign-In', user);
+    // $state.go('tabs.home');
+  };
+
 })
