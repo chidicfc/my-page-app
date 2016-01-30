b08961734faf659dc3c6f236d09527f46af8a7b1
chidi.uba <talk2chidicfc@yahoo.com>
2016-01-30 21:41:39 +0000

    update js

--- www/js/app.js
+++ www/js/app.js
@@ -73,7 +73,6 @@ angular.module('myPage', ['ionic'])
 })
 
 .controller('SignInCtrl', function($scope, $state) {
-
   $scope.signIn = function(user) {
     console.log('Sign-In', user);
     $state.go('tabs.home');
